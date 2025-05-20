23e97e2c507118a577b8f073111ba2c5c164f0f0
V1sandes <egorsizyh81@gmail.com>
2025-05-20 14:13:44 +0700

    wd

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 //Step 1 Count creatures
 
-    const count = data.length;
-    console.log(`Count: ${count}`);
+const count = data.length;
+console.log(`Count: ${count}`);
 
-    //Step 2 All castles
+//Step 2 All castles
 
-    const castles = [...new Set(data.map(creature => creature.castle))].sort().map(c => c.charAt(0).toUpperCase() + c.slice(1)).join(', ');
-    console.log(`Castles: ${castles}`);
+const castles = [...new Set(data.map(creature => creature.castle))].sort().map(c => c.charAt(0).toUpperCase() + c.slice(1)).join(', ');
+console.log(`Castles: ${castles}`);
 
-    //Step 3 Creature with large hp
+//Step 3 Creature with large hp
 
-    const largestHpCreature = data.reduce((prev, curr) => (curr.hp > prev.hp ? curr : prev)).creature;
-    console.log(`Largest hp: ${largestHpCreature}`);
+const largestHpCreature = data.reduce((prev, curr) => (curr.hp > prev.hp ? curr : prev)).creature;
+console.log(`Largest hp: ${largestHpCreature}`);
 
-    //Step 4 Average damage
+//Step 4 Average damage
 
-    const averageDamage = {};
-    data.forEach(creature => {
-    const avgDamage = (creature.damage.length === 1) ? creature.damage[0] : (creature.damage[0] + creature.damage[1]) / 2;
-    averageDamage[creature.creature] = avgDamage;
-    });
-    const averageDamageOutput = Object.entries(averageDamage)
-    .map(([creature, damage]) => `${creature}: ${damage}`)
-    .join(', ');
+const averageDamage = {};
+data.forEach(creature => {
+const avgDamage = (creature.damage.length === 1) ? creature.damage[0] : (creature.damage[0] + creature.damage[1]) / 2;
+averageDamage[creature.creature] = avgDamage;
+});
+const averageDamageOutput = Object.entries(averageDamage)
+.map(([creature, damage]) => `${creature}: ${damage}`)
+.join(', ');
 
 //Step 5 Strongest creature
 
-    const level7Creatures = data.filter(creature => creature.level === '7');
-    const strongestCreature = level7Creatures.reduce((prev, curr) => {
-    const avgDamagePrev = (prev.damage[0] + prev.damage[1]) / 2;
-    const avgDamageCurr = (curr.damage[0] + curr.damage[1]) / 2;
-    const attacksToKillPrev = Math.ceil(curr.hp / avgDamagePrev);
-    const attacksToKillCurr = Math.ceil(curr.hp / avgDamageCurr);
-    return (attacksToKillCurr < attacksToKillPrev) ? curr : prev;
-    }).creature;
-    
-    
+const level7Creatures = data.filter(creature => creature.level === '7');
+const strongestCreature = level7Creatures.reduce((prev, curr) => {
+const avgDamagePrev = (prev.damage[0] + prev.damage[1]) / 2;
+const avgDamageCurr = (curr.damage[0] + curr.damage[1]) / 2;
+const attacksToKillPrev = Math.ceil(curr.hp / avgDamagePrev);
+const attacksToKillCurr = Math.ceil(curr.hp / avgDamageCurr);
+return (attacksToKillCurr < attacksToKillPrev) ? curr : prev;
+}).creature;
 
-    
+
 
